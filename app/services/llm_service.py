a8a86acd963d8350b29f18355fde8319a7454bd5
Harish Arivananthan <harisharivananthan12@gmail.com>
2026-04-18 19:12:46 +0530

    LLM Integration

---
+++ app/services/llm_service.py
@@ -0,0 +1,23 @@
+import requests 
+from app.core.config import settings
+from app.core.logger import logger
+
+def generate_response(prompt:str):
+    try:
+        logger.info("Sending request to ollama")
+        response = requests.POST(
+            settings.OLLAMA_URL,
+            json={
+                "model":settings.MODEL,
+                "prompt":prompt,
+                "stream":False
+            }
+        )
+
+        data = response.json()
+        return data.get("response","no response")
+
+    except Exception as e:
+        logger.error(f"LLM error:{e}")
+        return "LLM connection Failed"
+        
---
+++ test_llm.py
@@ -0,0 +1,3 @@
+from app.services.llm_service import generate_response
+
+print(generate_response("Explain AI in one line"))
